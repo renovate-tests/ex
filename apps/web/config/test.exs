4b9dd7fdeefa6317892a49b00256bc85e3e64321
Keisuke Kan <9renpoto@gmail.com>
2019-01-03 17:13:00 +0900

    fix(.circleci) fixed cmds

--- apps/web/config/test.exs
+++ apps/web/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :web, Web.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_ENV_POSTGRES_USER"),
+  password: System.get_env("DB_ENV_POSTGRES_PASSWORD"),
   database: "web_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_ENV_POSTGRES_HOST"),
   pool: Ecto.Adapters.SQL.Sandbox
