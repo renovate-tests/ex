78f6f1f25b71108ca1de4cf08bf89e46944904f0
Keisuke Kan <9renpoto@gmail.com>
2019-01-03 16:21:10 +0900

    Added coverage report

--- mix.exs
+++ mix.exs
@@ -4,8 +4,16 @@ defmodule Ex2.MixProject do
   def project do
     [
       apps_path: "apps",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+      ],
+      test_coverage: [tool: ExCoveralls],
     ]
   end
 
@@ -15,6 +23,8 @@ defmodule Ex2.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.10", only: :test},
+    ]
   end
 end
